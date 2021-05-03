699e30b521f77ec7d281ed8be8979896323c5c3c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2021-05-02 21:23:46 -0400

    Update abilities.ts

--- data/mods/gen3hoenngaiden/abilities.ts
+++ data/mods/gen3hoenngaiden/abilities.ts
@@ -254,16 +254,58 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		rating: 3,
 		num: 146,
 	},
-	icebody: {
-		inherit: true,
+	icebod: {
+		onWeather(target, source, effect) {
+			if (effect.id === 'hail') {
+				this.heal(target.baseMaxhp / 16);
+			}
+		},
+		onImmunity(type, pokemon) {
+			if (type === 'hail') return false;
+		},
+		name: "Ice Bod",
+		rating: 1,
 	},
-	galvanize: {
-		inherit: true,
+	galvaniz: {
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && !noModifyType.includes(move.id) && !(move.isZ && move.category !== 'Status')) {
+				move.type = 'Electric';
+				move.galvanizeBoosted = true;
+			}
+		},
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.galvanizeBoosted) return this.chainModify([0x1333, 0x1000]);
+		},
+		name: "Galvaniz",
+		rating: 4,
 	},
-	overcoat: {
-		inherit: true,
+	overcoa: {
+		onImmunity(type, pokemon) {
+			if (type === 'sandstorm' || type === 'hail' || type === 'powder') return false;
+		},
+		onTryHitPriority: 1,
+		onTryHit(target, source, move) {
+			if (move.flags['powder'] && target !== source && this.dex.getImmunity('powder', target)) {
+				this.add('-immune', target, '[from] ability: Overcoat');
+				return null;
+			}
+		},
+		name: "Overcoa",
+		rating: 2,
 	},
-	magicguard: {
-		inherit: true,
+	magicguar: {
+		onDamage(damage, target, source, effect) {
+			if (effect.effectType !== 'Move') {
+				if (effect.effectType === 'Ability') this.add('-activate', source, 'ability: ' + effect.name);
+				return false;
+			}
+		},
+		name: "Magic Guar",
+		rating: 4,
 	},
 };
